f512db558ea8533a946365ef6b7d857e51df3c23
kyori19 <kyori@accelf.net>
2020-06-29 10:31:17 +0900

    Remove unneeded import

--- app/src/main/java/net/accelf/yuito/QuickTootHelper.java
+++ app/src/main/java/net/accelf/yuito/QuickTootHelper.java
@@ -3,7 +3,6 @@ package net.accelf.yuito;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
